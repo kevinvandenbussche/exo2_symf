ecf6fa5e03eefabff8628da7d62fd4f823fd0626
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2021-03-24 13:18:57 +0100

    formulaire admin

--- src/Controller/admin/admin_articles.php
+++ src/Controller/admin/admin_articles.php
@@ -4,6 +4,7 @@
 namespace App\Controller\admin;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine \ ORM \ EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,25 +21,13 @@ class admin_articles extends AbstractController
 //class abstractController
     public function insertArticle(EntityManagerInterface $entityManagerInterface)
     {
-//        je creer une class new Article que je mets dans une variable
         $article = new Article();
-//        j'insere le titre
-        $article->setTitle('article');
-//        j'insere le contenu
-        $article->setContent('blblablabla');
-//        j'inser la date de creation
-        $article->setCreateAt( new \DateTime('NOW'));
-//        j'insre un image
-        $article->setImage('coucou');
-//        j'insere le fait qu'il soit publier ou pas
-        $article->setIsPublished('true');
 
-//je dit a doctrine de memorise le nouvelle objet
-        $entityManagerInterface->persist($article);
-//        doctine envoi l'objet en base de donné
-        $entityManagerInterface->flush();
-//si les donnée sont bien envoyé j'envoi l'utilisateur sur une nouvelle page
-        return $this->render('admin.article.html.twig');
+        $form = $this->createForm(ArticleType::class, $article);
+
+        return $this->render('admin.article.html.twig',[
+            'articleView'=>$form->createView()
+        ]);
     }
 //je cree une nouvelle route et un nm pour ma fonction
     /**
